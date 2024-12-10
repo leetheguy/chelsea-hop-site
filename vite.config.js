4ed3c9a1ac2b21dc43503988d536a32adf9a3c8e
Lee Nathan <applesquash@gmail.com>
2024-12-09 18:50:26 -0800

    changed base

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/REPONAME/",
+  base: "/chelsea-hop-site/",
   plugins: [
     vue(),
     vueDevTools(),
