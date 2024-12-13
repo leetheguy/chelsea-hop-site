4d4ec72cbc6875c6b8c99f4943e171ebdb2f91b0
Lee Nathan <applesquash@gmail.com>
2024-12-13 10:25:04 -0800

    uploader

---
+++ src/views/UploadView.vue
@@ -0,0 +1,16 @@
+<script>
+	import { Dashboard } from '@uppy/vue';
+	import Uppy from '@uppy/core';
+	import Webcam from '@uppy/webcam';
+
+	// Don't forget the CSS: core and UI components + plugins you are using
+	import '@uppy/core/dist/style.css';
+	import '@uppy/dashboard/dist/style.css';
+	import '@uppy/webcam/dist/style.css';
+
+	const uppy = new Uppy().use(Webcam);
+</script>
+
+<template>
+	<Dashboard :uppy="uppy" />
+</template>
